91efaefe5d57aa5ae6781d6039e8b8cc29925bd1
sbilbrey1230 <sbamail100@gmail.com>
2025-04-27 22:41:44 -0400

    Setup basic format for main file and included necessary header files

---
+++ Main.cpp
@@ -0,0 +1,14 @@
+// Sean Bilbrey
+// CIS 1202 501
+// 4/27/2025
+
+#include<iostream>
+#include "Vehicle.h"
+#include "Car.h"
+#include "Truck.h"
+
+using namespace std;
+int main() {
+
+	return 0;
+}
